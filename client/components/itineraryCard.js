e1190fb29ebe63bbfa392edc3f1fbe3f264496d7
fowziasharmeen <fsharmeen@ymail.com>
2017-11-09 13:42:32 -0800

    anonymous home page complete

---
+++ client/components/itineraryCard.js
@@ -0,0 +1,12 @@
+angular.module('whatsGood')
+.component('itineraryCard', {
+  bindings: {
+  },
+  controller: function () {
+  },
+  template: `
+    <div>
+      user home is here
+    </div>
+`
+});
