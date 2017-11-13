de68f31e3fa936ad714b62dd3a71988ff241d046
Zac St Louis <p2kmafia@gmail.com>
2017-11-13 14:19:57 -0800

    added itincard init

--- client/app.js
+++ client/app.js
@@ -131,7 +131,7 @@ angular.module('whatsGood', ['ngMaterial', 'firebase', 'ngCookies'])
                     //userData = {user, wasCreated}
                     console.log('server confirmed login', userData);
                     lCtrl.answer(userData.data);
-                    lCtrl.showProgress = false;                    
+                    lCtrl.showProgress = false;
                   }, function(err) {
                     console.log('user auth on localhost failed', err);
                   });
@@ -245,6 +245,11 @@ angular.module('whatsGood', ['ngMaterial', 'firebase', 'ngCookies'])
             ctrl.user = user;
             ctrl.displayName = user.displayName;
             ctrl.isValidUser = true;
+<<<<<<< HEAD
+=======
+            ctrl.allItineraries = user.allItineraries;
+            ctrl.selectedItinerary = '' + ctrl.allItineraries[0].id;
+>>>>>>> added itincard init
           }, function () {
             console.log('canceled');
           });
